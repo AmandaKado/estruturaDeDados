734ca41d21a91990be1e069ffc5c59e1f07e5be1
Amanda Mayumi Kado <105023810+AmandaKado@users.noreply.github.com>
2026-06-16 18:11:03 -0300

    finalizado

--- hospital.py
+++ hospital.py
@@ -4,34 +4,34 @@ class Paciente:
         self.cor = cor
         self.proximo = None
 
-contador = None
-contadorVerde = 1
-contadorAmarelo = 201
+    contador = None
+    contadorVerde = 1
+    contadorAmarelo = 201
 
-def inserirVerde(paciente):
-    global contador
-    atual = contador
-    
-    while atual.proximo is not None: 
-        atual = atual.proximo
-    atual.proximo = paciente
-    
-def inserirAmarelo(paciente):
-    global contador
-
-    if contador.cor == "V":
-        paciente.proximo = contador
-        contador = paciente
-        return
-    
-    atual = contador
-    
-    while atual.proximo is not None and atual.proximo.cor == "A":   
-        atual = atual.proximo
+    def inserirVerde(paciente):
+        global contador
+        atual = contador
         
-    paciente.proximo = atual.proximo
-    atual.proximo = paciente
-    
+        while atual.proximo is not None: 
+            atual = atual.proximo
+        atual.proximo = paciente
+        
+    def inserirAmarelo(paciente):
+        global contador
+
+        if contador.cor == "V":
+            paciente.proximo = contador
+            contador = paciente
+            return
+        
+        atual = contador
+        
+        while atual.proximo is not None and atual.proximo.cor == "A":   
+            atual = atual.proximo
+            
+        paciente.proximo = atual.proximo
+        atual.proximo = paciente
+            
     def inserirCor():
         global contadorVerde, contadorAmarelo, contador
         
@@ -58,3 +58,55 @@ def inserirAmarelo(paciente):
             else:
                 inserirAmarelo(novoPaciente)
             print(f"Paciente {novoPaciente.numero}-{novoPaciente.cor} inserido na fila.")
+            
+    def imprimirListaEspera():
+        global contador
+
+        if contador is None:
+            print("A fila de espera está vazia.")
+            return
+
+        else:
+            atual = contador
+        
+        while contato is not None:
+            print(f"Paciente {atual.numero} - {atual.cor}")
+            atual = atual.proximo
+
+    def atenderPaciente():
+        global contador
+
+        if contador is None :
+            print("Não há pacientes para atender.")
+            return
+
+        pacienteAtendido = contador
+
+        contador = contador.proximo
+
+        print(f"Paciente {pacienteAtendido.numero} - {pacienteAtendido.cor} dirigir-se para sala de atendimento.")
+        
+    def menu():
+        while True:
+            print("\n*** SISTEMA DE TRIAGEM HOSPITALAR ***")
+            print("1 – Adicionar paciente à fila")
+            print("2 – Mostrar pacientes na fila")
+            print("3 – Chamar paciente")
+            print("4 – Fechar programa")
+            
+            opcao = input("Escolha uma opção: ").strip()
+            
+            if opcao == "1":
+                inserir()
+            elif opcao == "2":
+                imprimirListaEspera()
+            elif opcao == "3":
+                atenderPaciente()
+            elif opcao == "4":
+                print("Encerrando o programa...")
+                break
+            else:
+                print("Opção inválida! Tente novamente.")
+
+if __name__ == "__main__":
+    menu()
