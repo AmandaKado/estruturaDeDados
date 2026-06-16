96ea50a336ab4c1bb3071f4a849de6c58b9bbe1b
Amanda Mayumi Kado <105023810+AmandaKado@users.noreply.github.com>
2026-06-16 19:02:54 -0300

    arrumandovariavel

--- hospital.py
+++ hospital.py
@@ -4,27 +4,26 @@ class Paciente:
         self.cor = cor
         self.proximo = None
 
-contador = None
+primeiroFila = None
 contadorVerde = 1
 contadorAmarelo = 201
 
 def inserirVerde(paciente):
-    global contador
-    atual = contador
+    global primeiroFila
+    atual = primeiroFila
     
     while atual.proximo is not None: 
         atual = atual.proximo
     atual.proximo = paciente
     
 def inserirAmarelo(paciente):
-    global contador
+    global primeiroFila
 
-    if contador.cor == "V":
-        paciente.proximo = contador
-        contador = paciente
+    if primeiroFila is None:
+        primeiroFila = paciente
         return
     
-    atual = contador
+    atual = primeiroFila
     
     while atual.proximo is not None and atual.proximo.cor == "A":   
         atual = atual.proximo
@@ -33,68 +32,78 @@ def inserirAmarelo(paciente):
     atual.proximo = paciente
         
 def inserirCor():
-    global contadorVerde, contadorAmarelo, contador
-    
-    cor = input("Digite a cor (V para verde, A para amarelo): ").strip().upper()
-    
-    if cor != "V" and cor != "A":
-        print("Cor inválida. Digite apenas 'V' ou 'A'!")
-        return
-    elif cor == "V":
+    global contadorVerde, contadorAmarelo, primeiroFila
+
+    while True:
+        cor = (
+            input("\nDigite a cor (V para verde, A para amarelo): ")
+            .strip()
+            .upper()
+        )
+
+        if cor == "V" or cor == "A":
+            break
+
+        print("\n[ERRO] Cor inválida. Digite apenas 'V' ou 'A'!")
+
+    if cor == "V":
         numero = contadorVerde
         contadorVerde += 1
     else:
         numero = contadorAmarelo
         contadorAmarelo += 1
-        
+
     novoPaciente = Paciente(numero, cor)
 
-    if contador is None: 
-        contador = novoPaciente
-        print(f"Paciente {novoPaciente.numero} com cor {novoPaciente.cor} inserido na fila.") 
+    if primeiroFila is None:
+        primeiroFila = novoPaciente
+        print(
+            f"\nPaciente {novoPaciente.numero}-{novoPaciente.cor} inserido na fila."
+        )
     else:
         if cor == "V":
             inserirVerde(novoPaciente)
         else:
             inserirAmarelo(novoPaciente)
-        print(f"Paciente {novoPaciente.numero}-{novoPaciente.cor} inserido na fila.")
+        print(f"\nPaciente {novoPaciente.numero}-{novoPaciente.cor} inserido na fila.")
         
 def imprimirListaEspera():
-    global contador
+    global primeiroFila
 
-    if contador is None:
-        print("A fila de espera está vazia.")
+    if primeiroFila is None:
+        print("\nA fila de espera está vazia.")
         return
 
     else:
-        atual = contador
+        atual = primeiroFila
     
     while atual is not None:
         print(f"Paciente {atual.numero} - {atual.cor}")
         atual = atual.proximo
 
 def atenderPaciente():
-    global contador
+    global primeiroFila
 
-    if contador is None :
-        print("Não há pacientes para atender.")
+    if primeiroFila is None :
+        print("\nNão há pacientes para atender.")
         return
 
-    pacienteAtendido = contador
+    pacienteAtendido = primeiroFila
 
-    contador = contador.proximo
+    primeiroFila = primeiroFila.proximo
 
-    print(f"Paciente {pacienteAtendido.numero} - {pacienteAtendido.cor} dirigir-se para sala de atendimento.")
-    
+    print(f"\n[Paciente {pacienteAtendido.numero} - {pacienteAtendido.cor}] Por favor dirigir-se para sala de atendimento.")
+
 def menu():
     while True:
+        print("\n" + "_"*37)
         print("\n*** SISTEMA DE TRIAGEM HOSPITALAR ***")
-        print("1 – Adicionar paciente à fila")
+        print("\n1 – Adicionar paciente à fila")
         print("2 – Mostrar pacientes na fila")
         print("3 – Chamar paciente")
         print("4 – Fechar programa")
         
-        opcao = input("Escolha uma opção: ").strip()
+        opcao = input("\nEscolha uma opção: ").strip()
         
         if opcao == "1":
             inserirCor()
@@ -106,7 +115,7 @@ def menu():
             print("Encerrando o programa...")
             break
         else:
-            print("Opção inválida! Tente novamente.")
+            print("\nOpção inválida! Tente novamente.")
 
 if __name__ == "__main__":
     menu()
